fc1193769657604deab3d94af99447d017d21e89
igaguiar <igor_starting@hotmail.com>
2023-05-09 18:47:47 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Case Opener')
+
+@section('content')
+
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+
+@endsection
